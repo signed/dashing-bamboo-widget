1952bd7e33c69aec56cdf4cf208f9de318241cdd
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-10-25 21:49:34 +0100

    add a sample bamboo project

---
+++ test/bamboo/src/test/java/ut/org/example/MyComponentUnitTest.java
@@ -0,0 +1,17 @@
+package ut.org.example;
+
+import org.junit.Test;
+import org.example.api.MyPluginComponent;
+import org.example.impl.MyPluginComponentImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyComponentUnitTest
+{
+    @Test
+    public void testMyName()
+    {
+        MyPluginComponent component = new MyPluginComponentImpl(null);
+        assertEquals("names do not match!", "myComponent",component.getName());
+    }
+}
